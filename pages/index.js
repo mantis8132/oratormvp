cc11c63a9057d619c06b0ea146d46eeca3b2c10c
mantis8132 <mantis8132@gmail.com>
2025-06-08 22:54:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 export default function Home() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>🗣️ Orator MVP</h1>
-      <p>This is your pronunciation training tool. UI integration is in progress.</p>
-    </div>
+    <h1>Hello from Orator MVP!</h1>
   );
 }
